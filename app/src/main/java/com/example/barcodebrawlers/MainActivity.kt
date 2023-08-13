0f4e2e4edb9f2c43bfe41f0f87fbb1d30aedded4
Sam McMillan <smc397@uclive.ac.nz>
2023-08-13 18:35:17 +1200

    added basic navigation

--- app/src/main/java/com/example/barcodebrawlers/MainActivity.kt
+++ app/src/main/java/com/example/barcodebrawlers/MainActivity.kt
@@ -1,43 +1,72 @@
 package com.example.barcodebrawlers
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.barcodebrawlers.ui.theme.BarcodeBrawlersTheme
-
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BarcodeBrawlersTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun MyApp() {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BarcodeBrawlersTheme {
-        Greeting("Android")
+    NavHost(navController, startDestination = "screen1") {
+        composable("screen1") { Screen1(navController) }
+        composable("screen2") { Screen2(navController) }
+        composable("screen3") { Screen3(navController) }
     }
 }
+
+@Composable
+fun Screen1(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { navController.navigate("screen2") }) {
+            Text(text = "Go to Screen 2")
+        }
+    }
+}
+
+@Composable
+fun Screen2(navController: NavController) {
+    // Similar to Screen1 but with options to navigate to Screen1 and Screen3
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { navController.navigate("screen1") }) {
+            Text(text = "Go to Screen 1")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { navController.navigate("screen3") }) {
+            Text(text = "Go to Screen 3")
+        }
+    }
+}
+
+@Composable
+fun Screen3(navController: NavController) {
+    // Similar to Screen2 but with options to navigate to Screen1 and Screen2
+    // ...
+}
+
